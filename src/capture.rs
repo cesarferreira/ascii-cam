c6f52bf9aa098bf5443da4a3da5d438a1805bd17
cesarferreira <cesar.manuel.ferreira@gmail.com>
2026-05-31 01:53:05 +0100

    fix: fully restore pre-shortcut capture startup

--- src/capture.rs
+++ src/capture.rs
@@ -72,7 +72,6 @@ impl FfmpegCapture {
 
         let mut child = Command::new("ffmpeg")
             .args(&args)
-            .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
             .spawn()
